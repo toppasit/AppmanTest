014007080a614bda8866d7b8b09402405b8399ac
toppasit <toppst128@gmail.com>
2020-12-19 18:32:32 +0700

    add state for testing react hook

--- appman-pokedex-2018-master/src/pages/mainmenu/index.jsx
+++ appman-pokedex-2018-master/src/pages/mainmenu/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cardpage from '../card'
 import Dexpage from '../pokedex'
 
 const mainpage = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [add, setAdd] = useState(false)
+  console.log('add: ', add)
   const headerfont = {
-    font: "Gaegu",
+    font: "Gaegu"
   };
   return (
     <div>
       <h1 style={headerfont}>My Pokedex</h1>
+      <p onClick={() => setAdd(!add)}>Click me!</p>
     </div>
   )
 }
